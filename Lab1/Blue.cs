3e22f2c387c4eb07572dce75819374786a5e8e65
EgorNPC <baulor742@gmail.com>
2025-09-15 03:54:50 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+namespace Lab1
 {
     public class Blue
     {
@@ -7,38 +7,72 @@
             bool answer = false;
 
             // code here
+            if (a * b > 0)
+            {
+                answer = true;
+            }
+            else
+                answer = false;
 
-            // end
 
-            return answer;
+
+                // end
+
+                return answer;
         }
         public bool Task2(double d)
         {
             bool answer = false;
 
             // code here
+            if ((double)d != (int)d) 
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
             // code here
+            if (b!=0 && a%b==0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(d) > Math.Abs(f) && Math.Abs(d)> Math.Abs(g)) 
+            {
+                answer = d;
+            }
+            if (Math.Abs(f) > Math.Abs(d) && Math.Abs(f) > Math.Abs(g)) 
+            {
+                answer = f;
+            }
+            if (Math.Abs(g) > Math.Abs(f) && Math.Abs(g) > Math.Abs(d)) 
+            {
+                answer = g;
+            }
             // end
+
 
             return answer;
         }
@@ -47,7 +81,18 @@
             double answer = 0;
 
             // code here
-
+            if (x <= -1) 
+            {
+                answer = 0;
+            }
+            if (x>-1 && x<=0) 
+            {
+                answer = x + 1;
+            }
+            if (x > 0) 
+            {
+                answer = 1;
+            }
             // end
 
             return answer;
@@ -57,10 +102,17 @@
             bool answer = false;
 
             // code here
+            if (Math.Sqrt(squareS)/2 >= Math.Sqrt(circleS / Math.PI)) 
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
 
         public double Task7(double d, double f)
@@ -68,20 +120,37 @@
             int answer = 0;
 
             // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else if (f > 0)
+            {
+                answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
-            // end
+            if ((a / 2 + b / 2 + c / 2) >= 2 && ((a / 2 + b / 2 + c / 2)%3==0 || (a / 2 + b / 2 + c / 2)%3==2))
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            return answer;
+                return answer;
         }
     }
 }
